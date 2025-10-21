83da6d51277635076d612ca49f37943835483389
p738592 <p738592@WSAMZN-020NE9U2>
2025-10-21 19:15:24 +0530

    For Code Review from Copiolot

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,6 +1,13 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using ConsoleApp1;
+using Microsoft.Extensions.DependencyInjection;
 
 Console.WriteLine("Hello, World!");
-IService service = new Service();
+
+var serviceCollection = new ServiceCollection();
+serviceCollection.AddTransient<IService, Service>();
+var serviceProvider = serviceCollection.BuildServiceProvider();
+
+IService service = serviceProvider.GetService<IService>();
 service.Execute();
+service.Execute2();
--- ConsoleApp1/Service.cs
+++ ConsoleApp1/Service.cs
@@ -8,9 +8,21 @@ namespace ConsoleApp1
 {
     public class Service : IService
     {
+        private readonly IService2 service2;
+
+        public Service(IService2 service) 
+        {
+            service2 = service;
+        }
+
         public void Execute()
         {
             Console.WriteLine("Service is executing...");
+        }
+
+        public void Execute2()
+        {
+            service2.Execute(); 
         }
     }
 }
